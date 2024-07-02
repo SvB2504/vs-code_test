f19804ca4b605b22a7c9eb0fcf0523a654160a8c
= <=>
2024-07-02 15:45:12 +0200

    changed file names

--- backup-1.sh
+++ backup-1.sh
@@ -1,62 +1,25 @@
 #!/bin/bash
 
-# Set default values
-USER=$(whoami)
-HOMEFOLDER="/home/${USER}"
-SOURCE=$PWD
-DEST="/tmp/backups"
-
-# Get backup name
-echo "Enter a name for the backup:"
-read -p "==> " backup
-echo
-
-if [ -z "$backup" ]; then
-    echo "You need to give a name."
-    exit 1
-fi
-
-# Get source directory
-echo "Enter the directory to backup (default: current directory):"
-read -p "==> " source
-echo
-
-if [ -z "$source" ]; then
-    source=$SOURCE
+if [ ! -d /tmp/backups ] ; 
+then
+    echo Creating an backups folder in /tmp
+    mkdir /tmp/backups
 else
-    if [ ${source:0:1} = "~" ]
-    then
-        source=${HOMEFOLDER}${source:1}
-    fi
-    if [ ! -d "$source" ]; then
-        echo "This folder does not exist"
-        exit 1
-    fi
+    echo Backups folder already there
+fi
+echo Creating backup
+if [ -z $1 ]
+then
+    echo You need to give a parameter
+    exit 0
+fi
+if [ ! -d /tmp/backups/$1 ] ;
+then
+    mkdir /tmp/backups/$1
+    cp -r ~/* /tmp/backups/$1
+else
+    echo A backup with this name already exists, 
+    exit 0
 fi
 
-# Add trailing slash to source directory
-source+="/*"
-
-# Get destination directory
-echo "Enter the destination directory (default: /tmp/backups):"
-read -p "==> " dest
-echo
-
-if [ -z $dest ]; then
-    dest=$DEST
-fi
-
-if [ ! -d $dest ]; then
-    mkdir -p $dest
-fi
-
-# Create backup directory
-dest="${dest}/${backup}"
-if [ -d "$dest" ]; then
-    echo "A backup with this name already exists."
-    exit 1
-fi
-
-mkdir $dest
-echo "Creating backup..."
-cp -r $source $dest
+exit 0
